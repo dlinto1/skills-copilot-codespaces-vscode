01f5776c8ca1a705cf8346e8b51af63235dd3850
dlinto1 <140984746+dlinto1@users.noreply.github.com>
2023-08-01 05:34:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,62 @@
 // Create web server
-const server = http.createServer((req, res) => {
-    // Set CORS headers
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
 
-    // Process request
-    if (req.method === "POST") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            const parsed = JSON.parse(body);
+// Create web server
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create a database
+const commentsByPostId = {};
+
+// Create routes
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  // Get post id from url
+  const { id } = req.params;
+  // Get comment from body
+  const { content } = req.body;
+  // Get comments from database
+  const comments = commentsByPostId[id] || [];
+  // Add new comment to the database
+  comments.push({ id: commentId, content, status: 'pending' });
+  // Save comments to the database
+  commentsByPostId[id] = comments;
+  // Send event to event bus
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: { id: commentId, content, postId: id, status: 'pending' },
+  });
+  // Send response
+  res.status(201).send(comments);
+});
+app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
+  // Get event type
+  const { type, data } = req.body;
+  // Check if event type is CommentModerated
+  if (type === 'CommentModerated') {
+    // Get comment from the database
+    const { id, postId, status, content } = data;
+    // Get comments from the database
+    const comments = commentsByPostId[postId];
+    // Get comment from the database
+    const comment = comments.find((comment) => comment.id === id);
+    // Update comment status
+    comment.status = status;
+    // Send event to event bus
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: { id, postId, status, content },
+    });
+  }
+  // Send response
+  res.send
+});
